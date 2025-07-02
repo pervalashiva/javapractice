a36599ad812ffe3fadf660e65b01ece5d64f9743
pervalashiva <34185513+pervalashiva@users.noreply.github.com>
2025-07-01 23:37:05 -0700

    Added methods examples

---
+++ greeting.java
@@ -0,0 +1,19 @@
+package utils;
+
+public class greeting {
+	
+	public static void sayHello() 
+	{
+		System.out.println("Hello welcome to java");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		sayHello();
+		sayHello();
+		
+
+	}
+
+}
---
+++ methodreturn.java
@@ -0,0 +1,16 @@
+package utils;
+
+public class methodreturn {
+	public static int add(int a, int b) {
+		
+		return a + b;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int result = add(10, 10);
+		System.out.println("Sum is:" + result);
+	}
+
+}
