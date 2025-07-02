e749297ab7e4546c1ab1d4584f86326f8b9794f4
pervalashiva <34185513+pervalashiva@users.noreply.github.com>
2025-07-02 02:08:17 -0700

    OOPS practice

---
+++ Car.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class Car {
+    String color;     // property
+    int speed;        // property
+
+    // This is a method (behavior)
+    public void drive() {
+        System.out.println("The car is driving at " + speed + " km/h.");
+    }
+}
---
+++ Mainbike.java
@@ -0,0 +1,12 @@
+package utils;
+
+public class Mainbike {
+
+	  public static void main(String[] args) {
+	        Bike bike1 = new Bike("Red", 100);
+	        bike1.showDetails();
+
+	        Bike bike2 = new Bike("Blue", 120);
+	        bike2.showDetails();
+	    }
+}
