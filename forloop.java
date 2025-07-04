7e1fa5cf675c26c17842c7bd03063e14095c27e9
pervalashiva <34185513+pervalashiva@users.noreply.github.com>
2025-07-04 00:01:22 -0700

    java commit message

---
+++ forloop.java
@@ -0,0 +1,28 @@
+package utils;
+
+public class forloop {
+
+	public static void main(String[] args) {
+		
+//		for(int i=1; i<=10; i++) 
+//		{
+//			System.out.println(i);
+//		}
+//		
+//		for(int i =2; i <= 2; i+=2)
+//		{
+//			System.out.print(i);
+//		}
+
+		for(int i = 1; i<=10; i++)
+		{
+			if(i%2 == 0)
+			{
+				System.out.print("even");
+			}else
+			System.out.print("ODD");
+		}
+		
+	}
+
+}
---
+++ ternaryoperator.java
@@ -0,0 +1,24 @@
+package utils;
+
+public class ternaryoperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		int age = 20;
+//		String result = (age == 18) ? "Eligible to vote" : "Not eligible to vote";
+//		System.out.println(result);
+
+		
+		int a = 10;
+		int b = 20;
+		
+		a = a + b;
+		b = b - a;
+		a = a - b;
+		
+		
+		System.out.println("After swapping: num1 = " + a + ", num2 = " + b);
+	} 
+
+}
